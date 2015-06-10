e5cc1b61406029f4e8bc356d8c88479626318aab
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-06-10 19:02:09 +0200

    Using jQuery [Calendars Datepicker](http://keith-wood.name/calendarspicker.html) instead of default jQuery picker
    (1) Date fields can now be specified on either (current) Gregorian or (historic) Julian calendar.
    (2) As dates stored in Omeka should probably be consistent (i.e. modern Gregorian), a Julian date that was just entered/selected can be converted to Gregorian.
    (3) It is possible to specify either single dates or time spans.
    
    The current version will work with either English, German, or French localization configured in config.ini.
    
    Please note: The French i18n is incomplete, as the respective i18n terms from template.pot are still missing in fr.po / fr.mo (and will thus default to English). However, the calender sheets will show up in French due to the Calendars Datepicker i18n.
    
    Please also note: Adding additional calendar localizations requires to add respective jquery.calendars-XX.js and jquery.calendars.picker-XX.js files to the ./views/admin/javascripts folder. If the respective JavaScript files are missing, that will inevitably lead to a load error in Omeka.

--- views/admin/javascripts/date.js
+++ views/admin/javascripts/date.js
@@ -1,8 +1,111 @@
 jQuery(document).bind("omeka:elementformload", function() {
-    jQuery("input[data-type='date']").each(function() {
-        var format = jQuery(this).attr("data-format") || "yy-mm-dd";
-        jQuery(this).datepicker({
-            dateFormat: format,
-        });
-    });
+
+	var $ = jQuery; // use noConflict version of jQuery as the short $ within this block
+
+	$("input[data-type='date']").each(function() {
+
+		var thisHere=$(this);
+
+		var isSpan=(thisHere.val().indexOf(" - ")!=-1);
+
+		thisHere.closest(".input-block").css("width","100%");
+		thisHere.css("width","45%");
+
+		var format = thisHere.attr("data-format") || "yy-mm-dd";
+		thisHere.calendarsPicker({
+			dateFormat: format,
+			firstDay: 1,
+			yearRange: 'any',
+			rangeSelect: isSpan,
+			calendar: $.calendars.instance('gregorian', elTypesLocale),
+			showOnFocus: false,
+		});
+
+		var gredJulId = "gregJul"+thisHere.attr("id");
+		var timespanId = "timespan"+thisHere.attr("id");
+
+		thisHere.parent().children("#"+gredJulId).remove();
+
+		var isChecked = (isSpan ? "checked" : "");
+		thisHere.parent().append("<span id='"+gredJulId+"'> "+
+															"<button type='button' class='editGregLink'>"+
+																				elTypesGregorian.substr(0,1)+"</button>"+
+															"<button type='button' class='editJuliLink'>"+
+																				elTypesJulian.substr(0,1)+"</button>"+
+															"<input type='checkbox' id='"+timespanId+"' "+isChecked+"> "+elTypesTimeSpan+
+															"<br><strong>"+elTypesConvert+":</strong> "+
+															"<a href='#' class='convGregLink'>→ ["+elTypesGregorian+"]</a> "+
+															"<a href='#' class='convJuliLink'>→ ["+elTypesJulian+"]</a>"+
+															"</span>");
+
+		var thisDateEdit = thisHere.parent().find(".is-calendarsPicker");
+
+		$("#"+gredJulId+" .editGregLink").click( function() { return editGregJuli("gregorian"); } );
+		$("#"+gredJulId+" .editJuliLink").click( function() { return editGregJuli("julian"); } );
+
+		$("#"+timespanId).change(switchTimespan);
+
+		$("#"+gredJulId+" .convGregLink").click( function() { return convGregJuli("gregorian"); } );
+		$("#"+gredJulId+" .convJuliLink").click( function() { return convGregJuli("julian"); } );
+
+		// -----------------------------
+
+		function setGregJuli(to) {
+			var myCalendar = $.calendars.instance(to, elTypesLocale);
+			thisDateEdit.calendarsPicker("option", { calendar: myCalendar } );
+		}
+
+		// -----------------------------
+
+		function editGregJuli(what) {
+			setGregJuli(what);
+			thisDateEdit.calendarsPicker("show");
+			return false;
+		}
+
+		// -----------------------------
+
+		function switchTimespan() {
+			var isSpan = $(this).is(':checked');
+			thisDateEdit.calendarsPicker("option", { rangeSelect: isSpan } );
+
+			var curValue=thisDateEdit.val();
+
+			if (curValue) {
+				var curSep=curValue.indexOf(" - ");
+				var currentlySpan=(curSep!=-1);
+	
+				if (currentlySpan!=isSpan) {
+					if (isSpan) { thisDateEdit.val(curValue+" - "+curValue); }
+					else { thisDateEdit.val(curValue.substr(0,curSep)); }
+				}
+			}
+		}
+
+		// -----------------------------
+
+		function convGregJuli(to) {
+			var from=(to=='gregorian' ? 'julian' : 'gregorian');
+
+			setGregJuli(from);
+			thisDateEdit.calendarsPicker("show");
+
+			var curdate=thisDateEdit.calendarsPicker('getDate');
+
+			var toCalendar=$.calendars.instance(to, elTypesLocale);
+			var newdate=new Array();
+
+			for (var i = 0; i < curdate.length; i++) {
+				newdate[i] = toCalendar.fromJD(curdate[i].toJD());
+			}
+
+			setGregJuli(to);                                   
+			thisDateEdit.calendarsPicker('setDate', newdate);
+
+			return false;
+		}
+
+		// -----------------------------
+
+	});
 });
